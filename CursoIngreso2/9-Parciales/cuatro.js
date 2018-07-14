1dd0e120d6e03fa9fd5aa3453c484c75d0d8b396
milagros1010 <40776605+milagros1010@users.noreply.github.com>
2018-07-14 13:07:12 -0300

    Add files via upload
    clase2

---
+++ CursoIngreso2/9-Parciales/cuatro.js
@@ -0,0 +1,31 @@
+function mostrar()
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = prompt("ingrese primer numero");
+	numeroUno = parseInt(numeroUno);
+	numeroDos = prompt("ingrese segundo numero");
+	numeroDos = parseInt(numeroDos);
+
+	if (numeroUno==numeroDos) 
+	{
+		resultado = alert(numeroUno+""+numeroDos);
+	}
+	else if (numeroUno>numeroDos) 
+	{
+		resultado = alert(numeroUno - numeroDos);
+	}
+	else
+	{
+		resultado= alert(numeroUno+numeroDos);
+
+		resultado = (resultado>10);
+		alert(resultado+"la suma es"+resultado+"y supero el 10");
+
+	}
+
+	
+
+}
