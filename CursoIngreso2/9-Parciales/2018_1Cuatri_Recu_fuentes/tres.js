b09c0a4b9bd07d5e220f0085a84db376a6f2c413
milagros1010 <40776605+milagros1010@users.noreply.github.com>
2018-07-17 19:40:49 -0300

    Add files via upload

--- CursoIngreso2/9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ CursoIngreso2/9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,12 +1,23 @@
 function mostrar()
 {
-	var precio
-	var porcentaeDescuento
-	var iva
+	var precio;
+	var descuento;
+	var descuentoPrecio;
+	var PrecioDescuento;
+	var iva;
+	var pdi;
 
-	precio = prompt("ingrese precio")
-	precio = parseInt(precio)
-	porcentajeDescuento = prompt("ingrese porcentaje de descuento")
-	porcentajeDescuento	= prompt(porcentajeDescuento)
-	precio*porcentaeDescuento
+	precio = prompt("ingrese precio");
+	precio = parseInt(precio);
+	descuento = prompt("ingrese porcentaje de descuento");
+	descuento = parseInt(descuento);
+
+	descuentoPrecio = descuento;
+	descuento = precio*(descuento/100);
+	PrecioDescuento= precio-descuento;
+	iva = precio* (21/100);
+	pdi = PrecioDescuento + iva;
+	alert("-descuento en dinero:"+" "+descuentoPrecio+" "+"-precio con el descuento:"+" "+PrecioDescuento+" "+"-iva:"+" "+iva);
+	pdi = document.getElementById('elPrecioFinal').value=pdi;
+
 }
