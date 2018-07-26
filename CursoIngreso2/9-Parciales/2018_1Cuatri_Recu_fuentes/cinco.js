2f82851e0b45a1e1e67c75e8b6d7448c8731f845
milagros1010 <40776605+milagros1010@users.noreply.github.com>
2018-07-26 23:13:00 +0200

    Add files via upload
    n

--- CursoIngreso2/9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ CursoIngreso2/9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
 
+var dia;
+dia =prompt("ingrese dia de semana");
+
+switch(dia)
+{
+	case 'sabado':
+	case 'domingo':
+	alert(" buen finde");
+	break;
+	case 'lunes':
+	case 'martes':
+	case 'miercoles':
+	case 'jueves':
+	case 'viernes':
+	alert("a trabajar");
+	break;
+
+}
+
 }
