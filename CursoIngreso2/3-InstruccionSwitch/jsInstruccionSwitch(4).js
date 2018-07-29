dfbd209fdf12c7cffd76120a517075a9b55b7b8f
milagros1010 <40776605+milagros1010@users.noreply.github.com>
2018-07-29 19:08:40 -0300

    Add files via upload

--- CursoIngreso2/3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ CursoIngreso2/3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,25 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+switch(mesDelAño)	
+{
+	case 'enero':
+	case 'marzo':
+	case 'mayo':
+	case 'julio':
+	case 'agosto':
+	case 'octubre':
+	case 'diciembre':
+	alert("este mes tiene 31 días.");
+	break;
+	case 'febreo':
+	alert("Este mes tiene 28 días.");
+	break;
+
+	default:
+	alert("Este mes tiene 30 días.");
+
+}
 
 
 
