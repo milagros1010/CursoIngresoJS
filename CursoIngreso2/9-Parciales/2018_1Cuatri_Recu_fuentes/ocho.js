48adf7894dfcd7b6388f37682091d81ef39c1406
milagros1010 <40776605+milagros1010@users.noreply.github.com>
2018-08-07 16:25:25 -0300

    Add files via upload

--- CursoIngreso2/9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ CursoIngreso2/9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,91 @@
 function mostrar()
 {
 
+var nombre;
+var cantidadDePaginas;
+var cantidadDeVentas;
+var tema;	
+var contador; 
+var cantidadDeLibrosConPaginasPares;
+var cantidadDeLibrosConPaginasImpares;
+var cantidadDeVentasCero;
+var promedioDeVentas;
+var acumuladorDeVentas;
+var cantidadDeProgramacion;
+var acumuladorDePaginasDeProgramacion;
+
+acumuladorDeVentas=0;
+contador=0;
+respuesta= "si";
+cantidadDeLibrosConPaginasPares=0;
+cantidadDeLibrosConPaginasImpares=0;
+cantidadDeVentasCero=0;
+cantidadDeProgramacion=0;
+acumuladorDePaginasDeProgramacion=0;
+
+while(respuesta !="no")
+{
+
+
+while(contador<2)
+{
+
+	contador=contador+1;
+
+	nombre = prompt("ingrese nombre del libro");
+	cantidadDeVentas = prompt("ingrese cantidad de ventas");
+	cantidadDeVentas = parseInt(cantidadDeVentas);
+	acumuladorDeVentas = acumuladorDeVentas+1;
+
+	cantidadDePaginas = prompt("ingrese cantidad de paginas");
+	cantidadDePaginas = parseInt(cantidadDePaginas);
+
+	while (isNaN(cantidadDeVentas)||0>cantidadDeVentas)
+	{
+	cantidadDeVentas = prompt("Reingrese cantidad de ventas");
+	cantidadDeVentas = parseInt(cantidadDeVentas);
+	}	
+	
+	
+	tema = prompt("Reingrese tema");	
+	while(tema!="programacion" && tema!="robotica" && tema!="patrones " && tema!="bases de datos")
+	{
+		tema = prompt("Reingrese tema");
+	}
+
+	if (cantidadDePaginas%2==0) 
+	{
+		cantidadDeLibrosConPaginasPares = cantidadDeLibrosConPaginasPares+1;
+	}
+	else
+	{
+		cantidadDeLibrosConPaginasImpares = cantidadDeLibrosConPaginasImpares+1;
+	}	
+	if (cantidadDeVentas==0) 
+	{
+		cantidadDeVentasCero = cantidadDeVentasCero+1;
+	}
+	if (tema=="programacion") 
+	{
+		cantidadDeProgramacion = cantidadDeProgramacion+1;
+		acumuladorDePaginasDeProgramacion = acumuladorDePaginasDeProgramacion+1;
+	}
+
+respuesta = prompt("¿desea continuar?");
+
+}
+
+promedioDeVentas = acumuladorDeVentas/contador;
+
+document.write("la cantidad de libros pares es: "+cantidadDeLibrosConPaginasPares+"<br>");
+document.write("la cantidad de libros impares es: "+cantidadDeLibrosConPaginasImpares+"<br>");
+document.write("la cantidad de cero ventas: "+cantidadDeVentasCero+"<br>");
+document.write("el promedio de la ventas ingresadas es: "+promedioDeVentas+"<br>");
+document.write("la cantidad de paginas de programacion: "+acumuladorDePaginasDeProgramacion+"<br>");
+
+}
+
+
+
+
 }
--- CursoIngreso2/9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ CursoIngreso2/9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,106 +1,116 @@
 function mostrar()
 {
+ var nota;
+ var edad;
+ var sexo;
+ var sumaDeNotas;
+ var notaMasBaja;
+ var sexoDeNotaMasBaja;
+ var cantidadVaronesMayores;
+ var notaDelMasJoven;
+ var sexoDelMasJoven;
+ var edadDelMasJoven;
+ var cantidadDeEntradas;
+ var notaDeLaPrimeraMujerIngresada;
+ var bandera;
+ var contadorDeMujeres;
+ var mensaje;
 
-var nota;
-var contador;
-var sexo;
-var edad;
-var promedio;
-var acumuladorNotas;
-var notaMasBaja;
-var notaMasAlta;
-var sexoDeLaNotaMasBaja;
-var sexoDeLaNotaMasAlta;
-var varonesMayores;
-var contadorVarones;
+ contadorDeMujeres=0;
+ bandera=0;
+ sumaDeNotas=0;
+ contador=0;
+ cantidadVaronesMayores=0;
+ cantidadDeEntradas=0;
 
-contador=0;
-acumuladorNotas=0;
-contadorVarones=0;
 
 while(contador<2)
-{
-	contador= contador+1;
-
-
-	nota = prompt("ingrese nota");
-	nota = parseInt(nota);
-
-
-	while(isNaN(nota)||nota<0 || nota>10 )
 	{
-		nota = prompt("Reingrese nota");
-		nota = parseInt(nota);
 
+	contador=contador+1;
+
+ 	nota= prompt("ingrese nota");
+ 	nota= parseInt(nota);
+
+ 	while(isNaN(nota)||0<nota && nota>10)
+ 	{
+	nota= prompt("Error!!Reingrese nota");
+	nota= parseInt(nota); 	
+ 	}
+
+ 	edad= prompt("ingrese edad");
+ 	edad= parseInt(edad);
+
+ 	while(isNaN(edad)||0<edad && edad>100 )
+	{
+	edad= prompt("Reingrese edad");
+	edad= parseInt(edad);
 	}
 
-	sexo = prompt("ingrese sexo");
-
+	sexo=prompt("ingrese sexo")
 	while(sexo!="f" && sexo!="m")
 	{
-		sexo = prompt("Reingrese sexo");
-
+	sexo=prompt("Reingrese sexo");	
 	}
 
-		edad = prompt("ingrese edad");
-		edad = parseInt(edad);
+	
 
-	while(edad<0 || edad>100)
-	{
-		edad = prompt("	Reingrese edad");
-		edad = parseInt(edad);
-	}
-
-	if (contador==1) 	
+	if (contador==1) 
 	{
 		notaMasBaja = nota;
-		sexoDeLaNotaMasBaja = sexo;
-	 	notaMasAlta = nota;
-	 	sexoDeLaNotaMasAlta = sexo;
-	 
+		sexoDeNotaMasBaja = sexo;
+		edadDelMasJoven = edad;
+		sexoDelMasJoven = sexo;
+		notaDelMasJoven = nota;
 	}
 	else
+		{
+			if (nota<notaMasBaja) 
+			{
+				notaMasBaja = nota;
+				sexoDeNotaMasBaja = sexo;
+			}
+			
+	 		if (edad<edadDelMasJoven)
+			{
+				edadDelMasJoven = edad;
+				notaDelMasJoven = nota;
+				sexoDelMasJoven = sexo;
+			}
+
+
+		}
+	if (edad>=18 || sexo=="m" || nota>=6) 
 	{
-		if (nota<notaMasBaja) 
-		{
-			notaMasBaja = nota
-			sexoDeLaNotaMasBaja = sexo
-		}
+		cantidadVaronesMayores= cantidadVaronesMayores+1;
+	}
 
-		if (edad>=18 && sexo=="m" &&nota >=6) 
-		{
-			contadorVarones = contadorVarones+1;
-		}
-
-		else if (nota>notaMasAlta) 
-		{
-			notaMasAlta = nota;
-			sexoDeLaNotaMasAlta = sexo;
-		}
-
+	else if (sexo=="f" && bandera==0) 
+	{
+		notaDeLaPrimeraMujerIngresada=nota;
+		bandera=1;	
+	}
+	if (sexo=="f") 
+	{
+		contadorDeMujeres=contadorDeMujeres+1;
 	}
 
 
-
-acumuladorNotas = acumuladorNotas + nota;
-
-
-
-
-
-
-
-
+sumaDeNotas=sumaDeNotas+nota;
 
 
 }
 
-promedio = acumuladorNotas/contador;
+if (contadorDeMujeres==0) 
+{
+	mensaje = "no hubo ninguna mujer";
+}
+
+promedio = sumaDeNotas/contador;
 alert("el promedio es: "+promedio);
-alert("la nota mas baja es :"+notaMasBaja +" y el sexo es: " + sexoDeLaNotaMasBaja);
-alert("la nota mas alta es: "+notaMasAlta+" y el sexo es: "+sexoDeLaNotaMasAlta);
-alert("La cantidad de varones mayores a 18, que su nota haya sido mayor o igual a 6: "+contadorVarones);
-
-
-
+alert("la nota mas baja es: "+notaMasBaja+" y el sexo es: "+sexoDeNotaMasBaja);
+alert("La cantidad de varones mayores a 18, que su nota haya sido mayor o igual a 6: "+cantidadVaronesMayores);
+alert("El sexo de la persona mas joven es: " + sexoDelMasJoven+" y la nota del más joven: "+notaDelMasJoven);
+alert(" la nota de la primera mujer ingresada es: "+notaDeLaPrimeraMujerIngresada);
+alert(mensaje);
 }
